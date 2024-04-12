fe6cc4382c3f6a4b9eb1cff5312db0aed8178b1f
majitomoomin <mariajoseportill2@gmail.com>
2024-04-12 17:59:15 -0600

    commit 3#

---
+++ desafio_2/desafio_2/Clases.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_2
+{
+    public class NotificacionEmail : INotificable
+    {
+        public string DireccionCorreo { get; set; }
+
+        public NotificacionEmail(string direccionCorreo)
+        {
+            DireccionCorreo = direccionCorreo;
+        }
+
+        public void Notifica()
+        {
+            Console.WriteLine($"Enviando notificación por correo electrónico a {DireccionCorreo}");
+        }
+    }
+
+    public class NotificacionWhatsapp : INotificable
+    {
+        public string NumeroTelefono { get; set; }
+
+        public NotificacionWhatsapp(string numeroTelefono)
+        {
+            NumeroTelefono = numeroTelefono;
+        }
+
+        public void Notifica()
+        {
+            Console.WriteLine($"Enviando notificación por WhatsApp al número {NumeroTelefono}");
+        }
+    }
+
+    public class NotificacionSMS : INotificable
+    {
+        public string NumeroTelefono { get; set; }
+
+        public NotificacionSMS(string numeroTelefono)
+        {
+            NumeroTelefono = numeroTelefono;
+        }
+
+        public void Notifica()
+        {
+            Console.WriteLine($"Enviando notificación por SMS al número {NumeroTelefono}");
+        }
+    }
+
+
+}
---
+++ desafio_2/desafio_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Qué tipo de notificación te gustaría usar? (1. Email, 2. Whatsapp, 3. SMS)");
+            int tipoNotificacion = Convert.ToInt32(Console.ReadLine());
+
+            if (tipoNotificacion == 1)
+            {
+                Console.WriteLine("Ingrese la dirección de correo electrónico:");
+                string direccionCorreo = Console.ReadLine();
+                NotificacionEmail notificacionEmail = new NotificacionEmail(direccionCorreo);
+                notificacionEmail.Notifica();
+            }
+            else if (tipoNotificacion == 2)
+            {
+                Console.WriteLine("Ingrese el número de teléfono:");
+                string numeroTelefono = Console.ReadLine();
+                NotificacionWhatsapp notificacionWhatsapp = new NotificacionWhatsapp(numeroTelefono);
+                notificacionWhatsapp.Notifica();
+            }
+            else if (tipoNotificacion == 3)
+            {
+                Console.WriteLine("Ingrese el número de teléfono:");
+                string numeroTelefono = Console.ReadLine();
+                NotificacionSMS notificacionSMS = new NotificacionSMS(numeroTelefono);
+                notificacionSMS.Notifica();
+            }
+            else
+            {
+                Console.WriteLine("Tipo de notificación no reconocido.");
+            }
+        }
+    }
+
+}
